6e2d6325542a9907b07864d857c879533e01f67c
Muhammed Musab Güldemir <mmusabgldmr@gmail.com>
2024-12-08 12:38:31 +0300

    Lesson1

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -6,7 +6,41 @@ namespace Lesson1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+
+            //console.writline hızlıca yazmka istersek cw yapıp taba basacağız.!!!!!
+
+            //byte number = 2;
+            //int count = 10;
+            //float totalPrice = 25.50f; //C# Float olduğunu gösteriyoruz f koyarak
+            //char charcter = 'A';
+            //string firstName = "Musab";
+            //bool isWorking = true;
+
+            //var number = 2;
+            //var count = 10;
+            //var totalPrice = 25.50; //varın üzerine ctrl ve sol tıka basınca ne olduğunu gösterir bize
+            //var charcter = 'A';  
+            //var firstName = "Musab";
+            //var isWorking = true;
+
+            //ctrl x e basdığımız zaman satırı siler.
+
+            //Console.WriteLine("{0}  {1}" , byte.MinValue , byte.MaxValue);  //// Hangi aralıkta olduğunu gösterir 0 , 255 gibi
+            //Console.WriteLine("{0} {1}" , float.MinValue , float.MaxValue); //!!!!!!!!!
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(count);
+            //Console.WriteLine(totalPrice);
+            //Console.WriteLine(charcter);
+            //Console.WriteLine(firstName);
+            //Console.WriteLine(isWorking);
+
+            //const float Pi = 3.14f;
+            //Pi = 1;                       SOR!!!
+
+
+
+
         }
     }
 }
