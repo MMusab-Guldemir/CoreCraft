a3a2bc23d4450f43502bdf3f21ea0d59c4933ddb
Muhammed Musab Güldemir <mmusabgldmr@gmail.com>
2025-01-12 12:57:38 +0300

    Lesson6

---
+++ Lesson5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arrays
+
+            int number1;
+            int number2;
+            int number3;
+
+            //yerine
+
+            int[] numbers = new int[3] { 1, 2, 3 }; //süslü parantezi şu işe yarar.
+
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+
+            var numbers1 = new int[3];
+            numbers1[0] = 1;
+
+            Console.WriteLine(numbers1[0]); //1
+            Console.WriteLine(numbers1[1]); //0
+            Console.WriteLine(numbers1[2]); //0
+
+
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]); //true
+            Console.WriteLine(flags[1]); //false
+            Console.WriteLine(flags[2]); //false
+
+
+            var names = new string[3] { "john", "micheal", "rock" };
+
+
+        }
+    }
+}
---
+++ Lesson6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var firstName = "Mosh";
+            var lastName = "Hamedani";
+
+            var fullName = firstName + " " + lastName;
+            var myFullName = string.Format("My name is {0} {1} ", firstName, lastName);
+            ///yerlerini değiştirdik yani 0 first name oldu.
+            ///
+            Console.WriteLine(myFullName); // My name is Mosh Hamedani
+
+            var names = new string[3] { "John", "Jack", "Ninja" };
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames); //arlarına virgül koycak ve John, Jack, Ninja
+            // \n new line yani yeni satır alt satıra geçiyor tek satırda write gibi
+            var text = "Hi John\nLook into the following paths";
+            Console.WriteLine(text);
+            
+            var text1 = @"Hi John
+Look into the following paths";//otomatik olarak yapıyor yani et işareti koyunca
+        }
+    }
+}
